98c3c94edf98613b49559208b98be796062951c6
mollica-codes <mollica.26@osu.edu>
2022-04-22 18:13:24 +0000

    update model and admin

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Type', 'Year')
+    list_filter = ['Year']
+    search_fields = ['Name', 'Type']
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('Name', 'Description')
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -19,8 +19,32 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    Name = models.CharField(null=False, max_length=30)
+    Description = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return "Name: " + self.Name + "," + \
+               "Description: " + self.Description
 
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON')
+    ]
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    DealerID = models.IntegerField(default=0)
+    Name = models.CharField(max_length=20)
+    Type = models.CharField(max_length=20, choices=CHOICES)
+    Year = models.DateField(default=now)
+
+    def __str__(self):
+        return "Name: " + self.Name + "," + \
+               "Type: " + self.Type
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
